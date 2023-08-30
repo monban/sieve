a7f6ebaae0dd441281c25555dde3876f2a7a7baa
Ian Flynn <himitsu.monban@gmail.com>
2023-08-29 23:17:38 -0400

    run pi(n) for increasing powers of ten as proof it works

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"time"
 
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 )
 
 func main() {
-	var max uint64
-	var foundPrimes uint64
 	p := message.NewPrinter(language.English)
-	max = uint64(1_000_000_000)
-	bs := NewBoolStore(max)
+	max := 1_000_000_000
+	for n := 10; n <= max; n *= 10 {
+		start := time.Now()
+		primes := pi(uint64(n))
+		p.Printf("Found %d primes less than %d in %s.\n", primes, n, time.Since(start))
+	}
+}
 
-	fmt.Println()
-	for i := uint64(2); i < max; i++ {
+// pi(n) returns the number of primes less than n
+func pi(n uint64) uint64 {
+	var count uint64
+	bs := NewBoolStore(n)
+
+	for i := uint64(2); i < n; i++ {
 		if bs.Get(i) {
 			continue
 		}
-		// fmt.Println(i, "is prime")
-		foundPrimes++
-		for j := i * i; j < max; j += i {
+		count++
+		for j := i * i; j < n; j += i {
 			bs.Set(j)
 		}
 	}
+	return count
+}
 
-	// fmt.Println(bs)
-	p.Println("Found", foundPrimes, "primes less than", max)
+// findPrimes returns a slice containing all primes less than n
+func findPrimes(n uint64) []uint64 {
+	var foundPrimes []uint64
+	bs := NewBoolStore(n)
+
+	for i := uint64(2); i < n; i++ {
+		if bs.Get(i) {
+			continue
+		}
+		foundPrimes = append(foundPrimes, i)
+		for j := i * i; j < n; j += i {
+			bs.Set(j)
+		}
+	}
+	return foundPrimes
 }
